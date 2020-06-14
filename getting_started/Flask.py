89c006ace3f7199de1ea726b775b162296299c88
Aleksey Novikov <AlekseyNovikov65@gmail.com>
2020-06-14 21:37:46 +0300

    Начальная версия файлов

---
+++ getting_started/Flask.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/<username>", methods=['GET'])
+def index(username):
+       return "Hello, %s!" % username
+if __name__ == '__main__':
+       app.run(debug=True)
